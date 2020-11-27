dd26922b48897badd3707cb19bc3812d17ddba64
ThayanSivathevan <thayan.siva@gmail.com>
2020-11-27 18:08:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
-
+var port = process.env.PORT| 5000
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', process.env.PORT| 5000);
+app.set('port',port);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
+server.listen(port, function() {
   console.log('Starting server on port 5000');
 });
 
